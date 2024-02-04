ca4817608aec30ad3b378353d884a794b0ed7a19
ibrahim <ibrahim.taouaou@hotmail.com>
2024-02-04 13:39:09 +0100

    add cors

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,6 +1,7 @@
 import express from "express";
 import serverless from "serverless-http";
 import bodyParser from "body-parser";
+import { cors } from "cors";
 
 import { router as cardsRouter } from "../../src/routes/Card.js";
 import { router as homeRouter } from "../../src/routes/Home.js";
@@ -8,22 +9,13 @@ import { router as homeRouter } from "../../src/routes/Home.js";
 // id : 64867422
 
 const api = express();
-// const router = express.Router();
 
 const port = process.env.PORT || 3000;
 
-// router.get("/", (req, res) => {
-//   res.json("Hello 😊");
-// });
-
 // Middleware
+api.use(cors());
 api.use("/api/cards", cardsRouter);
 api.use("/api", homeRouter);
 api.use(bodyParser.json());
-// api.use(bodyParser.json()).use("/.netlify/functions/api", router);
-
-// api.listen(port, () =>
-//   console.log(`Notre apili Node est démarré sur : http://localhost:${port}`)
-// );
 
 export const handler = serverless(api);
