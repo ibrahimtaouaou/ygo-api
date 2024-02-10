bbd5f9f1483d8ac7497b2d1fdda030e4c06a92d8
ibrahim <ibrahim.taouaou@hotmail.com>
2024-02-10 23:40:40 +0100

    small changes

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -15,7 +15,6 @@ const port = process.env.PORT || 3000;
 // Middleware
 api.use(bodyParser.json());
 api.options("*", cors());
-api.use(cors());
 api.use("/api/cards", cardsRouter);
 api.use("/api", homeRouter);
 
