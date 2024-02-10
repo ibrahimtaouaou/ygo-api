5820f489fdc7ecd5f5b42bb7ddf1ed4bcc38cf3b
ibrahim <ibrahim.taouaou@hotmail.com>
2024-02-10 11:50:31 +0100

    local file and MVC vjsp

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -14,11 +14,13 @@ const port = process.env.PORT || 3000;
 
 // Middleware
 api.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173/*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 api.use(bodyParser.json());
-api.use(cors());
+// api.use(cors());
 api.use("/api/cards", cardsRouter);
 api.use("/api", homeRouter);
 
